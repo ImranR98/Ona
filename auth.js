d72d1047ab9dfaa124986627e6e3e4e6ccf73b90
Imran Remtulla <iremtulla15@gmail.com>
2020-04-19 18:35:36 -0400

    bug

--- auth.js
+++ auth.js
@@ -7,9 +7,15 @@ const functions = require('./functions') // Import the functions file
 const variables = require('./variables') // Import the variables file
 
 // Get the password from the database
-module.exports.getPassword = async () => await functions.getSingleItemByIdFromMongo(variables.url, variables.configdb, variables.authCollection, 'password')
+module.exports.getPassword = async () => {
+    let password = await functions.getSingleItemByIdFromMongo(variables.url, variables.configdb, variables.authCollection, 'password')
+    if (password) password = password.password // Quality readable code
+    return password
+}
+
 // Check if the provided password matches the one in the database
 module.exports.authenticate = async (password) => bcrypt.compareSync(password, await this.getPassword())
+
 // Update the password or add it if one doesn't exist
 module.exports.updateAuth = async (password) => {
     let encryptedPassword = bcrypt.hashSync(password, 10)
