8bd08199aec5947a18f7592b2c1aa42654b908bd
Imran Remtulla <iremtulla15@gmail.com>
2020-05-03 01:06:33 -0400

    Basically works, needs polishing and performance improvements

--- client/src/app/gallery/gallery.component.ts
+++ client/src/app/gallery/gallery.component.ts
@@ -14,17 +14,10 @@ export class GalleryComponent implements OnInit, OnDestroy {
 
   constructor(private activatedRoute: ActivatedRoute, private router: Router, private apiService: ApiService, private errorService: ErrorService) { }
 
-  sortForm = new FormGroup({
-    sort: new FormControl(0)
-  })
-
   loading = false
 
   collection
-
-  maxPages = 0
-  pageSize = 100
-  sorts = ['Date (Desc.)', 'Date (Asc.)', 'Name (Desc.)', 'Name (Asc.)']
+  loadAtATime = 50
 
   subs: Subscription[] = []
   thumbnails = []
@@ -32,19 +25,9 @@ export class GalleryComponent implements OnInit, OnDestroy {
   listSource = new BehaviorSubject([])
   list = this.listSource.asObservable()
 
-  pageSource = new BehaviorSubject(0)
-  page = this.pageSource.asObservable()
-
   ngOnInit(): void {
-    this.sortForm.controls['sort'].valueChanges.subscribe(selectedSort => {
-      this.listSource.next(this.sortList(selectedSort, this.listSource.value))
-    })
-
     this.subs.push(this.list.subscribe(list => {
-      this.updateThumbnails()
-    }))
-
-    this.subs.push(this.page.subscribe(page => {
+      this.thumbnails = []
       this.updateThumbnails()
     }))
 
@@ -57,7 +40,7 @@ export class GalleryComponent implements OnInit, OnDestroy {
         this.loading = true
         this.apiService.list(this.collection).then(data => {
           this.loading = false
-          this.listSource.next(this.sortList(this.sortForm.controls['sort'].value, data))
+          this.listSource.next(this.sortList(0, data))
         }).catch(err => {
           alert(this.errorService.stringifyError(err))
           this.router.navigate(['/choice'])
@@ -66,10 +49,15 @@ export class GalleryComponent implements OnInit, OnDestroy {
     }))
   }
 
+  onScroll() {
+    this.loading = true
+    this.updateThumbnails()
+    this.loading = false
+  }
+
   sortList(selectedSort: number, list) {
     list = list.filter(item => {
       if (!item.DateTimeOriginal) {
-        console.log(item)
         return false
       }
       return true
@@ -83,29 +71,13 @@ export class GalleryComponent implements OnInit, OnDestroy {
     return list
   }
 
-  next() {
-    if (this.pageSource.value < this.maxPages) this.pageSource.next(this.pageSource.value + 1)
-  }
-
-  prev() {
-    if (this.pageSource.value > 0) this.pageSource.next(this.pageSource.value - 1)
-  }
-
-  toPage(num: number) {
-    if (num <= this.maxPages && num >= 0) this.pageSource.next(num)
-  }
-
   updateThumbnails() {
-    this.thumbnails = []
-    this.maxPages = Math.ceil(this.listSource.value.length / this.pageSize)
-    let startIndex = this.pageSource.value * this.pageSize
-    let endIndex = startIndex + this.pageSize - 1
-    let listMaxIndex = this.listSource.value.length - 1
-    if (startIndex <= listMaxIndex) {
-      if (endIndex > listMaxIndex) endIndex = listMaxIndex
-      for (let i = startIndex; i <= endIndex; i++) {
-        this.apiService.single(this.collection, this.listSource.value[i]._id).then(res => this.thumbnails[i - startIndex] = res).catch(err => console.log(err))
-      }
+    let startIndex = this.thumbnails.length == 0 ? 0 : this.thumbnails.length - 1
+    let endIndex = this.listSource.value.length - 1 > startIndex + this.loadAtATime - 1 ? startIndex + this.loadAtATime - 1 : this.listSource.value.length
+    if (startIndex != endIndex) {
+      this.apiService.many(this.collection, this.listSource.value.filter((el, index) => index >= startIndex && index <= endIndex).map(el => el._id)).then(res => {
+        this.thumbnails = this.thumbnails.concat(res)
+      }).catch(err => console.log(err))
     }
   }
 
