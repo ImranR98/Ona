b0e113124d174a488e2326a7a722fcf32187aaeb
Imran Remtulla <iremtulla15@gmail.com>
2020-12-18 17:44:49 -0500

    Update to Angular 11

--- client/src/app/single-item/single-item.component.spec.ts
+++ client/src/app/single-item/single-item.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SingleItemComponent } from './single-item.component';
 
@@ -6,7 +6,7 @@ describe('SingleItemComponent', () => {
   let component: SingleItemComponent;
   let fixture: ComponentFixture<SingleItemComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SingleItemComponent ]
     })
